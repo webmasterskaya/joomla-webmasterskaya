cf8dae24c76145fded22a7b969075a2c794e85e8
Artem Vasilev <kern.usr@gmail.com>
2021-07-06 17:37:19 +0300

    first commit

---
+++ webmasterskaya.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package    System - Webmasterskaya
+ * @version    __DEPLOY_VERSION__
+ * @author     Artem Vasilev - webmasterskaya.xyz
+ * @copyright  Copyright (c) 2018 - 2021 Webmasterskaya. All rights reserved.
+ * @license    MIT, see LICENSE.txt
+ * @link       https://webmasterskaya.xyz/
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Plugin\CMSPlugin;
+
+class PlgSystemWebmasterskaya extends CMSPlugin
+{
+	/**
+	 * Affects constructor behavior. If true, language files will be loaded automatically.
+	 *
+	 * @var    boolean
+	 * @since  1.0.0
+	 */
+	protected $autoloadLanguage = true;
+
+	public function onAfterInitialise()
+	{
+		$paths = [
+			'ProductionCalendar' => JPATH_LIBRARIES
+				. '/lib_production_calendar/ProductionCalendar',
+		];
+		foreach ($paths as $class => $path)
+		{
+			if (is_dir($path))
+			{
+				JLoader::registerNamespace(
+					'Webmasterskaya\\' . $class,
+					$path,
+					false,
+					false,
+					'psr4'
+				);
+			}
+		}
+	}
+}
